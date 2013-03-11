6f759b72b3ab0d30d58de0cbfd68677530f89f4b
liudi <liudi@pset.suntec.net>
2013-03-10 20:02:24 -0700

    add script to manage git

---
+++ getscripts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/majutsushi/tagbar.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/Lokaltog/vim-powerline.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/Valloric/YouCompleteMe.git
